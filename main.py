d66a8c08d6cfb3759cf6dbe4f09d90922364d6f9
Ivan Korniichuk <85750192+ivan-korniichuk@users.noreply.github.com>
2024-03-27 13:55:00 +0000

    updated multiprocessing
    Solved all issues with multiprocessing and object detection. Reduced execution time by 60% per frame

--- main.py
+++ main.py
@@ -1,78 +1,106 @@
 import cv2 as cv
 import numpy as np
-from image_processor import ImageProcessor
+from image_processor_new import ImageProcessor
+
+from multiprocessing.connection import Client
+import subprocess
+
 from visualiser import Visualiser
 import time
-# from multiprocessing import Process
 from data_analiser import DataAnaliser
 
-# for solar panels
-# WIDTH_HEIGHT = (3000, 2300)
-WIDTH_HEIGHT = (3000, 2000)
-OFFSET = (750, 500)
-DIST_COEFFS = np.array([-3.4, -0.2, 0.015, 0, 0.05])
-CAMERA_MATRIX = np.array([[5000, 0, 970],
-                          [0, 5000, 550],
-                          [0, 0, 1]]) 
+if __name__ == "__main__":
 
-RESERVED_AREA_B = [(0,0), (450,450)]
-RESERVED_AREA_Y = ((2550,0), (3000, 450))
-# DROP_OFFS_Y = [[(2550,1550), (3000,2000)],[(0, 775), (450, 1225)]]
-# DROP_OFFS_B = [[(0,1550), (450,2000)],[(2550, 775), (3000, 1225)]]
-DROP_OFF_B_MID = [(2550, 775), (3000, 1225)]
-DROP_OFF_B_COR = [(0,1550), (450,2000)]
-DROP_OFF_Y_MID = [(0, 775), (450, 1225)]
-DROP_OFF_Y_COR = [(2550,1550), (3000,2000)]
-# SOLAR_PANELS_Y = [(), (), ()]
-# SOLAR_PANELS_B = [(), (), ()]
-# SOLAR_PANELS_G = [(), (), ()]
-SIMA_AREA_Y = [(1500, 0), (1950, 150)]
-SIMA_AREA_B = [(1050, 0), (1500, 150)]
+    WIDTH_HEIGHT = (3000, 2000)
+    OFFSET = (750, 500)
+    DIST_COEFFS = np.array([-3.4, -0.2, 0.015, 0, 0.05])
+    CAMERA_MATRIX = np.array([[5000, 0, 970],
+                            [0, 5000, 550],
+                            [0, 0, 1]])
 
-cap = cv.VideoCapture(1)
+    RESERVED_AREA_B = [(0, 0), (450, 450)]
+    RESERVED_AREA_Y = ((2550, 0), (3000, 450))
 
-while True:
-    print("2")
-    ret, frame = cap.read()
-    if ret:
-        image_processor = ImageProcessor(
-            frame, 
-            WIDTH_HEIGHT,
-            OFFSET,
-            CAMERA_MATRIX,
-            DIST_COEFFS
-        )
-        if image_processor.perspective_transform is not None:
-            break
+    DROP_OFF_B_MID = [(2550, 775), (3000, 1225)]
+    DROP_OFF_B_COR = [(0, 1550), (450, 2000)]
+    DROP_OFF_Y_MID = [(0, 775), (450, 1225)]
+    DROP_OFF_Y_COR = [(2550, 1550), (3000, 2000)]
+    # SOLAR_PANELS_Y = [(), (), ()]
+    # SOLAR_PANELS_B = [(), (), ()]
+    # SOLAR_PANELS_G = [(), (), ()]
+    SIMA_AREA_Y = [(1500, 0), (1950, 150)]
+    SIMA_AREA_B = [(1050, 0), (1500, 150)]
 
-print("calibrated")
-visualiser = Visualiser(DROP_OFF_B_MID, DROP_OFF_Y_MID, DROP_OFF_B_COR, DROP_OFF_Y_COR, RESERVED_AREA_B, RESERVED_AREA_Y, SIMA_AREA_B, SIMA_AREA_Y)
-data_analiser = DataAnaliser(DROP_OFF_B_MID, DROP_OFF_Y_MID, DROP_OFF_B_COR, DROP_OFF_Y_COR, RESERVED_AREA_B, RESERVED_AREA_Y)
+    visualiser = Visualiser(DROP_OFF_B_MID, DROP_OFF_Y_MID, DROP_OFF_B_COR, DROP_OFF_Y_COR, RESERVED_AREA_B,
+                            RESERVED_AREA_Y, SIMA_AREA_B, SIMA_AREA_Y)
+    data_analiser = DataAnaliser(DROP_OFF_B_MID, DROP_OFF_Y_MID, DROP_OFF_B_COR, DROP_OFF_Y_COR, RESERVED_AREA_B,
+                                RESERVED_AREA_Y)
 
-while True:
-    ret, frame = cap.read()
-    times_1 = time.time_ns()
-    # img = image_processor.get_transformed_img(frame)
-    # this one takes 0.1 sec but adapts to camera changes
-    img = image_processor.adapt_and_calibrate_img(frame)
-    times_2 = time.time_ns()
-    plants = image_processor.get_plants(img)
-    # plants = image_processor.get_plants(img)
-    times_3 = time.time_ns()
-    time1 = times_2 - times_1
-    time2 = times_3 - times_2
-    data_analiser.update(plants)
-    plants = data_analiser.cluster_plants()
-    print("model time")
-    print(time2)
-    print("img time")
-    print(time1)
-    visualiser.update(img, plants)
-    if ret:
-        cv.imshow("frame", visualiser.view)
+    cap = cv.VideoCapture(1)
+    while True:
+        ret, frame = cap.read()
+        if ret:
+            image_processor = ImageProcessor(
+                frame, 
+                WIDTH_HEIGHT,
+                OFFSET,
+                CAMERA_MATRIX,
+                DIST_COEFFS
+            )
+            if image_processor.perspective_transform is not None:
+                break
 
-    if cv.waitKey(1) == ord("q"):
-        break
-    
-cap.release()
-cv.destroyAllWindows()
+    print("calibrated")
+    plants_detector_process = subprocess.Popen(['python3', 'plants_detector.py'])
+    time.sleep(2)
+
+    address = ('localhost', 6001)
+    retries = 5
+    delay = 1  # Delay in seconds
+    img = frame
+    # 
+    cv.namedWindow("frame1", cv.WINDOW_NORMAL)   # Create window with freedom of dimensions
+    # 
+
+    for attempt in range(retries):
+        try:
+            with Client(address, authkey=b'secret password') as conn:
+                while True:
+                    ret, frame = cap.read()
+                    time1 = time.time_ns()
+                    conn.send(img)
+                    time2 = time.time_ns()
+                    # img = image_processor.get_transformed_img(frame)
+                    img = image_processor.adapt_and_calibrate_img(frame)
+                    img2 = img.copy()
+                    time3 = time.time_ns()
+                    plants = conn.recv()
+                    time4 = time.time_ns()
+                    data_analiser.update(plants)
+                    plants = data_analiser.cluster_plants()
+                    visualiser.update(img2, plants)
+                    time5 = time.time_ns()
+                    print("send time")
+                    print(time2 - time1)
+                    print("receive time")
+                    print(time4 - time3)
+                    print("img processing time")
+                    print(time3 - time2)
+                    print("visualiser processing")
+                    print(time5-time4)
+                    # 
+                    # cv.resizeWindow("frame1", 600,400)
+                    # 
+                    cv.imshow("frame1", visualiser.view)
+
+                    if cv.waitKey(1) == ord("q"):
+                        conn.send("close")
+                        # plants_detector_process.terminate()  # Ensure code2 is terminated
+                        # plants_detector_process.wait()  # Wait for the process to finish
+                        break
+        except (ConnectionRefusedError, EOFError) as e:
+            print(f"Attempt {attempt + 1}: {str(e)}")
+            time.sleep(delay)
+    # plants_detector_process.terminate()  # Ensure code2 is terminated
+    # plants_detector_process.wait()  # Wait for the process to finish
+    raise Exception("Failed to connect to code2")
---
+++ plants_detector.py
@@ -0,0 +1,44 @@
+from multiprocessing.connection import Listener
+import time
+from ultralytics import YOLO
+
+model = YOLO('models/i900model8500.pt')
+
+def get_plants (img):
+    plants = []
+    results = model.predict(img, conf=0.4,classes=0, verbose=False)
+
+    for xywh in results[0].boxes.xywh:
+        # append only x and y coords of the center
+        plants.append((int(xywh[0]), int(xywh[1])))
+    return plants
+
+def listen_plant_detector():
+    print("STARTED")
+    address = ('localhost', 6001)
+    listener = Listener(address, authkey=b'secret password')
+    print("Listener started in plants_detector2.py")
+
+    conn = listener.accept()  # Accept a connection
+    print("Connection accepted in plants_detector2.py")
+
+    while True:
+        try:
+            img = conn.recv()
+            time1 = time.time_ns()
+            result = get_plants(img)
+            print("plants")
+            print(len(result))
+            conn.send(result)
+            print("model time")
+            print(time.time_ns() - time1)
+        except EOFError:
+            print("Connection closed by client")
+            conn = listener.accept()  # Wait for a new connection
+            print("Connection accepted in code2")
+
+    # conn.close()
+    # listener.close()
+
+if __name__ == '__main__':
+    listen_plant_detector()

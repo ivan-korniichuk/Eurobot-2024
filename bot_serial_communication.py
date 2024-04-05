17d21da701b7f052e1979356a80dcec1c559b508
highjeans <highjeans11@hotmail.com>
2024-04-05 16:26:32 +0100

    Update bot_serial_communication.py

--- bot_serial_communication.py
+++ bot_serial_communication.py
@@ -29,6 +29,9 @@ class MainBotSerialCommunication:  # TODO: To be updated as needed
         angle_to = int((pi + atan2(start[1] - end[1], start[0] - end[0])) * 100)
         angle_from = int((angle_from + pi) * 100)
         byte_val = (start[0] << 88) + (start[1] << 72) + (end[0] << 56) + (end[1] << 40) + (angle_from << 24) + (angle_to << 8) + end_speed
-        self.locomotion.write(byte_val.to_bytes(13, signed=False))
+        self.locomotion.write(byte_val.to_bytes(14, signed=False))
+
+    def stopBot(self):
+        self.locomotion.write(1)
 
 
